bdd8c1402b8557270f73a970c2c3fb89f131ba5d
Saad <saadadnan94@gmail.com>
2018-03-05 10:13:22 -0600

    added threads part 1

--- ptcount_mutex.c
+++ ptcount_mutex.c
@@ -77,6 +77,9 @@ int main(int argc, char *argv[])
     targs->loop = loop;
     targs->inc = inc;
     /* Make call to pthread_create here */
+    pthread_create(&threads[i],&attr,inc_count, (void*)targs);
+
+
   }
 
   /* Wait for all threads to complete using pthread_join.  The threads
@@ -84,10 +87,11 @@ int main(int argc, char *argv[])
    */
   for (i = 0; i < NUM_THREADS; i++) {
     /* Make call to pthread_join here */
+    pthread_join(threads[i],NULL);
   }
 
   printf ("Main(): Waited on %d threads. Final value of count = %d. Done.\n",
-          NUM_THREADS, count);
+      NUM_THREADS, count);
 
   /* Clean up and exit */
   pthread_attr_destroy(&attr);
